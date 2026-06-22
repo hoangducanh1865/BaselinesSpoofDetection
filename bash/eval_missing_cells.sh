625711b0381fdbe6d084cd5bdf30a6505e7040c3
hoangducanh1865 <hoangducanh.1865@gmail.com>
2026-06-22 10:01:22 +0700

    Limit missing eval to MoLEx 2019LA rows

--- bash/eval_missing_cells.sh
+++ bash/eval_missing_cells.sh
@@ -71,6 +71,26 @@ export NES2NET_EVAL_NUM_WORKERS=${NES2NET_EVAL_NUM_WORKERS:-8}
 export RAWTFNET_EVAL_BATCH_SIZE=${RAWTFNET_EVAL_BATCH_SIZE:-128}
 export RAWTFNET_EVAL_NUM_WORKERS=${RAWTFNET_EVAL_NUM_WORKERS:-16}
 
+reset_eval_env() {
+  export PYTHONUNBUFFERED=1
+
+  export XLSR2_300M_PATH=/home/user14/anhhd/spoof/pretrained_ssl_models/xlsr2_300m__s3prl__converted_ckpts/pytorch_model.bin
+  export WAVLM_LARGE_PATH=/home/user14/anhhd/spoof/pretrained_ssl_models/wavlm_large__mrdragonfox__llase_g1/pytorch_model.bin
+
+  export MOLEX_EVAL_BATCH_SIZE=${MOLEX_EVAL_BATCH_SIZE:-128}
+  export MOLEX_EVAL_NUM_WORKERS=${MOLEX_EVAL_NUM_WORKERS:-16}
+  export AASIST_EVAL_BATCH_SIZE=${AASIST_EVAL_BATCH_SIZE:-128}
+  export AASIST_EVAL_NUM_WORKERS=${AASIST_EVAL_NUM_WORKERS:-16}
+  export XLSR_SLS_EVAL_BATCH_SIZE=${XLSR_SLS_EVAL_BATCH_SIZE:-8}
+  export XLSR_SLS_EVAL_NUM_WORKERS=${XLSR_SLS_EVAL_NUM_WORKERS:-8}
+  export W2V2_AASIST_EVAL_BATCH_SIZE=${W2V2_AASIST_EVAL_BATCH_SIZE:-8}
+  export W2V2_AASIST_EVAL_NUM_WORKERS=${W2V2_AASIST_EVAL_NUM_WORKERS:-8}
+  export NES2NET_EVAL_BATCH_SIZE=${NES2NET_EVAL_BATCH_SIZE:-16}
+  export NES2NET_EVAL_NUM_WORKERS=${NES2NET_EVAL_NUM_WORKERS:-8}
+  export RAWTFNET_EVAL_BATCH_SIZE=${RAWTFNET_EVAL_BATCH_SIZE:-128}
+  export RAWTFNET_EVAL_NUM_WORKERS=${RAWTFNET_EVAL_NUM_WORKERS:-16}
+}
+
 activate_env() {
   local env_name=$1
   set +u
@@ -101,6 +121,7 @@ run_eval() {
   fi
 
   activate_env "$env_name"
+  reset_eval_env
 
   echo
   echo "================================================================================"
@@ -167,71 +188,71 @@ run_or_continue molex_anhhd molex asvspoof2019la "$MOLEX_CKPT_2019LA_AVG" molex_
 run_or_continue molex_anhhd molex vsasv          "$MOLEX_CKPT_2019LA_AVG" molex_2019la_avg
 run_or_continue molex_anhhd molex asvspoof5      "$MOLEX_CKPT_2019LA_AVG" molex_2019la_avg
 
-# Train ASVspoof5 -- best: missing 2019LA, VSASV, ASV5.
-run_or_continue molex_anhhd molex asvspoof2019la "$MOLEX_CKPT_ASV5_BEST" molex_asv5_best
-run_or_continue molex_anhhd molex vsasv          "$MOLEX_CKPT_ASV5_BEST" molex_asv5_best
-run_or_continue molex_anhhd molex asvspoof5      "$MOLEX_CKPT_ASV5_BEST" molex_asv5_best
-
-# Train ASVspoof5 -- averaged: missing ITW, 2019LA, VSASV, ASV5.
-run_or_continue molex_anhhd molex in_the_wild    "$MOLEX_CKPT_ASV5_AVG" molex_asv5_avg
-run_or_continue molex_anhhd molex asvspoof2019la "$MOLEX_CKPT_ASV5_AVG" molex_asv5_avg
-run_or_continue molex_anhhd molex vsasv          "$MOLEX_CKPT_ASV5_AVG" molex_asv5_avg
-run_or_continue molex_anhhd molex asvspoof5      "$MOLEX_CKPT_ASV5_AVG" molex_asv5_avg
-
-# =============================================================================
-# 2) Strong SSL baselines: XLSR-SLS, W2V2-AASIST, Nes2Net.
-# =============================================================================
-
-# XLSR-SLS pretrained 2019LA: missing VLSP, VSASV, ASV5.
-run_or_continue nes2net_anhhd xlsr_sls vlsp2025  "$XLSR_SLS_CKPT_2019" xlsr_sls_2019la
-run_or_continue nes2net_anhhd xlsr_sls vsasv     "$XLSR_SLS_CKPT_2019" xlsr_sls_2019la
-run_or_continue nes2net_anhhd xlsr_sls asvspoof5 "$XLSR_SLS_CKPT_2019" xlsr_sls_2019la
-
-# W2V2-AASIST pretrained 2019LA: missing VLSP, VSASV, ASV5.
-run_or_continue nes2net_anhhd wav2vec2_aasist vlsp2025  "$W2V2_AASIST_CKPT_2019" w2v2_aasist_2019la
-run_or_continue nes2net_anhhd wav2vec2_aasist vsasv     "$W2V2_AASIST_CKPT_2019" w2v2_aasist_2019la
-run_or_continue nes2net_anhhd wav2vec2_aasist asvspoof5 "$W2V2_AASIST_CKPT_2019" w2v2_aasist_2019la
-
-# Nes2Net pretrained 2019LA: missing VLSP, VSASV, ASV5.
-run_or_continue nes2net_anhhd nes2net vlsp2025  "$NES2NET_CKPT_2019" nes2net_2019la
-run_or_continue nes2net_anhhd nes2net vsasv     "$NES2NET_CKPT_2019" nes2net_2019la
-run_or_continue nes2net_anhhd nes2net asvspoof5 "$NES2NET_CKPT_2019" nes2net_2019la
-
-# Nes2Net pretrained ASV5: missing VLSP, VSASV, ASV5.
-run_or_continue nes2net_anhhd nes2net vlsp2025  "$NES2NET_CKPT_ASV5" nes2net_asv5
-run_or_continue nes2net_anhhd nes2net vsasv     "$NES2NET_CKPT_ASV5" nes2net_asv5
-run_or_continue nes2net_anhhd nes2net asvspoof5 "$NES2NET_CKPT_ASV5" nes2net_asv5
-
-# =============================================================================
-# 3) AASIST family.
-# =============================================================================
-
-# AASIST pretrained 2019LA: missing VSASV, ASV5.
-run_or_continue nes2net_anhhd aasist vsasv     "$AASIST_CKPT_2019" aasist_2019la
-run_or_continue nes2net_anhhd aasist asvspoof5 "$AASIST_CKPT_2019" aasist_2019la
-
-# AASIST pretrained ASV5: all table cells are missing. This skips if checkpoint absent.
-run_or_continue nes2net_anhhd aasist vlsp2025       "$AASIST_CKPT_ASV5" aasist_asv5
-run_or_continue nes2net_anhhd aasist dfadd_test     "$AASIST_CKPT_ASV5" aasist_asv5
-run_or_continue nes2net_anhhd aasist fake_or_real   "$AASIST_CKPT_ASV5" aasist_asv5
-run_or_continue nes2net_anhhd aasist in_the_wild    "$AASIST_CKPT_ASV5" aasist_asv5
-run_or_continue nes2net_anhhd aasist asvspoof2019la "$AASIST_CKPT_ASV5" aasist_asv5
-run_or_continue nes2net_anhhd aasist vsasv          "$AASIST_CKPT_ASV5" aasist_asv5
-run_or_continue nes2net_anhhd aasist asvspoof5      "$AASIST_CKPT_ASV5" aasist_asv5
-
-# AASIST-L pretrained 2019LA: missing VSASV, ASV5.
-run_or_continue nes2net_anhhd aasist_l vsasv     "$AASIST_L_CKPT_2019" aasist_l_2019la
-run_or_continue nes2net_anhhd aasist_l asvspoof5 "$AASIST_L_CKPT_2019" aasist_l_2019la
-
-# =============================================================================
-# 4) RawTFNet.
-# =============================================================================
-
-# RawTFNet pretrained 2019LA: missing VLSP, 2019LA, VSASV, ASV5.
-run_or_continue nes2net_anhhd rawtfnet vlsp2025       "$RAWTFNET_CKPT" rawtfnet_2019la
-run_or_continue nes2net_anhhd rawtfnet asvspoof2019la "$RAWTFNET_CKPT" rawtfnet_2019la
-run_or_continue nes2net_anhhd rawtfnet vsasv          "$RAWTFNET_CKPT" rawtfnet_2019la
-run_or_continue nes2net_anhhd rawtfnet asvspoof5      "$RAWTFNET_CKPT" rawtfnet_2019la
+# # Train ASVspoof5 -- best: missing 2019LA, VSASV, ASV5.
+# run_or_continue molex_anhhd molex asvspoof2019la "$MOLEX_CKPT_ASV5_BEST" molex_asv5_best
+# run_or_continue molex_anhhd molex vsasv          "$MOLEX_CKPT_ASV5_BEST" molex_asv5_best
+# run_or_continue molex_anhhd molex asvspoof5      "$MOLEX_CKPT_ASV5_BEST" molex_asv5_best
+#
+# # Train ASVspoof5 -- averaged: missing ITW, 2019LA, VSASV, ASV5.
+# run_or_continue molex_anhhd molex in_the_wild    "$MOLEX_CKPT_ASV5_AVG" molex_asv5_avg
+# run_or_continue molex_anhhd molex asvspoof2019la "$MOLEX_CKPT_ASV5_AVG" molex_asv5_avg
+# run_or_continue molex_anhhd molex vsasv          "$MOLEX_CKPT_ASV5_AVG" molex_asv5_avg
+# run_or_continue molex_anhhd molex asvspoof5      "$MOLEX_CKPT_ASV5_AVG" molex_asv5_avg
+#
+# # =============================================================================
+# # 2) Strong SSL baselines: XLSR-SLS, W2V2-AASIST, Nes2Net.
+# # =============================================================================
+#
+# # XLSR-SLS pretrained 2019LA: missing VLSP, VSASV, ASV5.
+# run_or_continue nes2net_anhhd xlsr_sls vlsp2025  "$XLSR_SLS_CKPT_2019" xlsr_sls_2019la
+# run_or_continue nes2net_anhhd xlsr_sls vsasv     "$XLSR_SLS_CKPT_2019" xlsr_sls_2019la
+# run_or_continue nes2net_anhhd xlsr_sls asvspoof5 "$XLSR_SLS_CKPT_2019" xlsr_sls_2019la
+#
+# # W2V2-AASIST pretrained 2019LA: missing VLSP, VSASV, ASV5.
+# run_or_continue nes2net_anhhd wav2vec2_aasist vlsp2025  "$W2V2_AASIST_CKPT_2019" w2v2_aasist_2019la
+# run_or_continue nes2net_anhhd wav2vec2_aasist vsasv     "$W2V2_AASIST_CKPT_2019" w2v2_aasist_2019la
+# run_or_continue nes2net_anhhd wav2vec2_aasist asvspoof5 "$W2V2_AASIST_CKPT_2019" w2v2_aasist_2019la
+#
+# # Nes2Net pretrained 2019LA: missing VLSP, VSASV, ASV5.
+# run_or_continue nes2net_anhhd nes2net vlsp2025  "$NES2NET_CKPT_2019" nes2net_2019la
+# run_or_continue nes2net_anhhd nes2net vsasv     "$NES2NET_CKPT_2019" nes2net_2019la
+# run_or_continue nes2net_anhhd nes2net asvspoof5 "$NES2NET_CKPT_2019" nes2net_2019la
+#
+# # Nes2Net pretrained ASV5: missing VLSP, VSASV, ASV5.
+# run_or_continue nes2net_anhhd nes2net vlsp2025  "$NES2NET_CKPT_ASV5" nes2net_asv5
+# run_or_continue nes2net_anhhd nes2net vsasv     "$NES2NET_CKPT_ASV5" nes2net_asv5
+# run_or_continue nes2net_anhhd nes2net asvspoof5 "$NES2NET_CKPT_ASV5" nes2net_asv5
+#
+# # =============================================================================
+# # 3) AASIST family.
+# # =============================================================================
+#
+# # AASIST pretrained 2019LA: missing VSASV, ASV5.
+# run_or_continue nes2net_anhhd aasist vsasv     "$AASIST_CKPT_2019" aasist_2019la
+# run_or_continue nes2net_anhhd aasist asvspoof5 "$AASIST_CKPT_2019" aasist_2019la
+#
+# # AASIST pretrained ASV5: all table cells are missing. This skips if checkpoint absent.
+# run_or_continue nes2net_anhhd aasist vlsp2025       "$AASIST_CKPT_ASV5" aasist_asv5
+# run_or_continue nes2net_anhhd aasist dfadd_test     "$AASIST_CKPT_ASV5" aasist_asv5
+# run_or_continue nes2net_anhhd aasist fake_or_real   "$AASIST_CKPT_ASV5" aasist_asv5
+# run_or_continue nes2net_anhhd aasist in_the_wild    "$AASIST_CKPT_ASV5" aasist_asv5
+# run_or_continue nes2net_anhhd aasist asvspoof2019la "$AASIST_CKPT_ASV5" aasist_asv5
+# run_or_continue nes2net_anhhd aasist vsasv          "$AASIST_CKPT_ASV5" aasist_asv5
+# run_or_continue nes2net_anhhd aasist asvspoof5      "$AASIST_CKPT_ASV5" aasist_asv5
+#
+# # AASIST-L pretrained 2019LA: missing VSASV, ASV5.
+# run_or_continue nes2net_anhhd aasist_l vsasv     "$AASIST_L_CKPT_2019" aasist_l_2019la
+# run_or_continue nes2net_anhhd aasist_l asvspoof5 "$AASIST_L_CKPT_2019" aasist_l_2019la
+#
+# # =============================================================================
+# # 4) RawTFNet.
+# # =============================================================================
+#
+# # RawTFNet pretrained 2019LA: missing VLSP, 2019LA, VSASV, ASV5.
+# run_or_continue nes2net_anhhd rawtfnet vlsp2025       "$RAWTFNET_CKPT" rawtfnet_2019la
+# run_or_continue nes2net_anhhd rawtfnet asvspoof2019la "$RAWTFNET_CKPT" rawtfnet_2019la
+# run_or_continue nes2net_anhhd rawtfnet vsasv          "$RAWTFNET_CKPT" rawtfnet_2019la
+# run_or_continue nes2net_anhhd rawtfnet asvspoof5      "$RAWTFNET_CKPT" rawtfnet_2019la
 
 echo
 echo "[INFO] Done. Logs: $LOG_ROOT"
