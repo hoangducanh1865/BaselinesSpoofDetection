888b9815064fd166ae415ff19699d9e882689890
hoangducanh1865 <hoangducanh.1865@gmail.com>
2026-06-22 20:54:24 +0700

    feat: update

--- bash/eval_missing_cells.sh
+++ bash/eval_missing_cells.sh
@@ -329,7 +329,7 @@ echo "[INFO] WAVLM_LARGE_PATH=$WAVLM_LARGE_PATH"
 
 # Done: VLSP EER=54.548.
 # run_or_continue moef_cu113 moef vlsp2025       "$MOEF_CKPT_2019LA" moef_2019la_pretrained
-run_or_continue moef_cu113 moef dfadd_test     "$MOEF_CKPT_2019LA" moef_2019la_pretrained
+# run_or_continue moef_cu113 moef dfadd_test     "$MOEF_CKPT_2019LA" moef_2019la_pretrained
 # Done: FoR EER=44.087.
 # run_or_continue moef_cu113 moef fake_or_real   "$MOEF_CKPT_2019LA" moef_2019la_pretrained
 # Done: ITW EER=35.621.
@@ -340,7 +340,7 @@ run_or_continue moef_cu113 moef dfadd_test     "$MOEF_CKPT_2019LA" moef_2019la_p
 # Done: 2019LA EER=13.939.
 # run_or_continue molex_anhhd molex asvspoof2019la "$MOLEX_CKPT_ASV5_AVG" molex_asv5_avg
 
-run_or_continue moef_cu113 moef vsasv          "$MOEF_CKPT_2019LA" moef_2019la_pretrained
+# run_or_continue moef_cu113 moef vsasv          "$MOEF_CKPT_2019LA" moef_2019la_pretrained
 # run_or_continue moef_cu113 moef asvspoof5      "$MOEF_CKPT_2019LA" moef_2019la_pretrained
 
 # =============================================================================
@@ -367,7 +367,7 @@ run_or_continue moef_cu113 moef vsasv          "$MOEF_CKPT_2019LA" moef_2019la_p
 # run_or_continue molex_anhhd molex in_the_wild    "$MOLEX_CKPT_2019LA_AVG" molex_2019la_avg
 # run_or_continue molex_anhhd molex asvspoof2019la "$MOLEX_CKPT_2019LA_AVG" molex_2019la_avg
 # run_or_continue molex_anhhd molex vsasv          "$MOLEX_CKPT_2019LA_AVG" molex_2019la_avg
-# run_or_continue molex_anhhd molex asvspoof5      "$MOLEX_CKPT_2019LA_AVG" molex_2019la_avg
+run_or_continue molex_anhhd molex asvspoof5      "$MOLEX_CKPT_2019LA_AVG" molex_2019la_avg
 
 # MoLEx Train ASVspoof5 rows not requested for this run:
 # run_or_continue molex_anhhd molex vsasv     "$MOLEX_CKPT_ASV5_BEST" molex_asv5_best
